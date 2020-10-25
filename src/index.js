abf59f080bf6d89ce08ffe84b1f95473603afb79
MishaNero <70752946+MishaNero@users.noreply.github.com>
2020-10-25 17:59:55 +0300

    init: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result;
+  if (str.length % 2 !== 0) {
+      result = false;
+  }
+
+  const map = new Map(bracketsConfig);
+  let bracket = [];
+  const array = str.split('');
+  array.forEach((el, i) => {
+      if (map.has(el) && !(el === bracket[bracket.length - 1] && el === map.get(el))) {bracket.push(el);} 
+      else if (el === map.get(bracket[bracket.length - 1])) {bracket.pop();}
+  });
+
+  if (bracket.length === 0 && str.length % 2 === 0) {
+      result = true;
+  } else {
+      result = false;
+  }
+  console.log(result, bracket);
+  return result;
 }
